ce1abfbef5f6eec994a0c4fbf89086896dd0706f
Noah <eeue56@users.noreply.github.com>
2016-05-31 17:13:05 +0100

    use latest version of snapnow

--- snapnow.sh
+++ snapnow.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 current_date=$(date)
+date_without_spaces=$(echo $current_date | sed "s/ //g")
+out_path="${HOME}/Desktop/${date_without_spaces}.png"
 
 ## imagemagick provies this import command
-import "~/Desktop/${current_date}.png"
+import "${out_path}"
